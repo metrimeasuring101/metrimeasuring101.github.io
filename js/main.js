fb21dd7a69493ce27ce204cd94b47ccfe8765c56
Chloe Liau <chloeliau@Chloes-MacBook-Pro.local>
2015-07-19 08:03:53 -0700

    Attempt to modify logged in images/text/logout button.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(function(){
 	var btn_login = '<a href="#" id="login" class="ion-social-facebook"></i></a>';
 
 	var div_session = "<div id='facebook-session'>"
-					+ "<strong></strong>"
-					+ "<img>"
+					+ "<strong class='argh'></strong>"
+					+ "<img class='argh'>"
 					+ "<a href='#' id='logout' class='btn btn-danger'> Log out</a>"
 					+ "<div>";
 
